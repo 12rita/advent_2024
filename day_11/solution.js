6ed92c020ec8bdabbbf4d9df564c7b6dfba54351
ma.k.goncharova <ma.k.goncharova@tbank.ru>
2024-12-13 19:15:42 +0300

    Day 11 second half done

--- day_11/solution.js
+++ day_11/solution.js
@@ -1,89 +1,68 @@
 import {input} from "./input.js";
-import {testInput} from "./test.js";
 
 const inputSwitch = input;
 
-const zeros = [];
-const nines = [];
-const map = inputSwitch.split('\n').map((el, line) => {
-    const row = el.split('');
-    row.forEach((el, row) => {
-        if (el === '0') {
-            zeros.push({y: line, x: row})
-        }
-        else if (el === '9') {
-            nines.push({y: line, x: row})
-        }
-    })
-    return row
-});
+const splitStringHalf = (str) => {
+    return [str.slice(0, str.length / 2), String(Number(str.slice(str.length / 2)))]
+}
 
-const m = map.length, n = map[0].length;
-
-const bfs = (start, finish) => {
-    const stack = [];
-    const visited = new Set();
-    stack.push(start);
-    visited.add(start);
-    while (stack.length > 0) {
-        const current = stack.shift();
-        const {x, y} = current
-        if (x === finish.x && y === finish.y) {
-            return true
-        }
-        if (y - 1 >= 0 && !visited.has(map[y - 1][x]) && +map[y - 1][x] - map[y][x] === 1) stack.push({x, y: y - 1})
-        if (y + 1 < m && !visited.has(map[y + 1][x]) && +map[y + 1][x] - map[y][x] === 1) stack.push({x, y: y + 1})
-        if (x - 1 >= 0 &&!visited.has(map[y][x - 1]) && +map[y][x - 1] - map[y][x] === 1) stack.push({x: x - 1, y})
-        if (x + 1 < n &&!visited.has(map[y][x + 1]) && +map[y][x + 1] - map[y][x] === 1) stack.push({x: x + 1, y})
+const transform = (arr) => {
+    const local = [...arr];
+    for (let i = 0; i < local.length; i++) {
+        if (local[i] === "0") local[i] = "1";
+        else if (local[i].length % 2 === 0) {
+            local.splice(i, 1, ...splitStringHalf(local[i]))
+            i++
+        } else local[i] = String(+local[i] * 2024)
     }
-    return false
+
+    // console.log({arr},{local})
+    return local
 }
 
-const zerosScore = () => {
-    let result = 0;
-    zeros.forEach((start) => {
-        result += nines.filter((point)=>bfs(start,point)).length
+const rocks = inputSwitch.split(" ");
 
-    })
-    return result;
+const cycle = () => {
+    let localRocks = rocks.slice();
+    for (let i = 0; i < 25; i++) {
+        localRocks = transform(localRocks)
+    }
+    return localRocks
 }
 
-const result = zerosScore();
-console.log(result)
+const f = Date.now()
+const result = cycle();
+const diff = Date.now() - f
+console.log(result.length, diff)
 
 
 /*--------------------Part2-----------------------------------------*/
+const cache = new Map();
 
-let paths = {};
+const recursiveTransform = (elem, step, goal) => {
+    if (cache.has(`${elem},${step}`)) return cache.get(`${elem},${step}`);
 
-const traverse = (x, y, path) => {
-    const val = +map[y][x];
-    if (val === 9) {
-        paths[path] = 1;
-        return;
-    }
-    if (y - 1 >= 0 && +map[y - 1][x] - val === 1) {
-        traverse(x, y - 1, path + 'U')
-    }
-    if (y + 1 < m && +map[y + 1][x] - val === 1) {
-        traverse(x, y + 1, path + 'D')
-    }
-    if (x - 1 >= 0 && +map[y][x - 1] - val === 1) {
-        traverse(x - 1, y, path + 'L')
-    }
-    if (x + 1 < n && +map[y][x + 1] - val === 1) {
-        traverse(x + 1, y, path + 'R')
-    } else return;
-}
-
-const zerosRating = ()=>{
     let result = 0;
-    zeros.forEach((start) => {
-        traverse(start.x, start.y, '')
-        result+=Object.keys(paths).length;
-        paths = {};
-    })
+    if (step === goal) return 1;
+    else {
+        const res = transform([elem]);
+        for (let i = 0; i < res.length; i++) {
+            result += recursiveTransform(res[i], step + 1, goal)
+        }
+    }
+    cache.set(`${elem},${step}`, result);
     return result;
 }
-const result2 = zerosRating();
-console.log(result2)
+
+const cycle2 = () => {
+
+    return [...rocks].reduce((acc, el) => {
+        return acc + recursiveTransform(el, 0, 75)
+    }, 0)
+}
+const f2 = Date.now()
+
+
+const result2 = cycle2();
+const diff2 = Date.now() - f
+console.log(result2, diff2)
